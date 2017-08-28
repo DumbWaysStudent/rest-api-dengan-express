fcaed932680e0b0de0545a29b9f9ff0cdfbfcda7
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2017-08-28 13:52:37 +0700

    5. Membuat Routers

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 const express = require('express');
 
 const app = express();
+const routers = require('./routers');
 
-app.get('/api/students', function(req, res){
-  console.log('this is homepage');
-  res.send({
-    name: "Ega Radiegtya",
-    age: 27
-  })
-});
+app.use('/api', routers);
 
 app.listen(process.env.port || 5000, function(){
   console.log('express app now listening requests');
